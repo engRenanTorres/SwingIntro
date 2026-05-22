d9aecea669c22a4d503766dc3fcad0f49c3f730b
Renan de Souza Torres <renan.torres@prof.infnet.edu.br>
2026-05-22 07:21:34 -0300

    Comentario em ingles

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Olá Seja Bem vindo!");
+        System.out.printf("Welcome!");
+
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
